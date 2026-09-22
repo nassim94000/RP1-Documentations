c8c0eaf6cdb538b16f012a770337d41c095c9d4e
SIOpRaphael <raphael.pichegru.sio@gmail.com>
2026-09-22 11:01:32 +0200

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -9,32 +9,46 @@ maridb -h localhost -u johndoe -p;
 use synchro;
 
 
-
-CREATE TABLE tache (
-  id SERIAL PRIMARY KEY,
-  id_utilisateur FOREIGN KEY REFERENCES utilisateur (id_utilisateur),
-  libelle VARCHAR(100) NOT NULL,
-  description VARCHAR(500),
-  date_debut DATETIME NOT NULL
-  date_fin DATETIME NOT NULL,
-  importance VARCHAR(10) CHECK (importance IN ('faible', 'moyen', 'fort')) NOT NULL,
-  statut VARCHAR(10) CHECK (statut IN ('en cours', 'fait', 'en retard')) NOT NULL
+CREATE TABLE utilisateur (
+    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    fonction VARCHAR(20) CHECK (fonction IN ('operationnel', 'chef de projet', 'ressources humaines')) NOT NULL,
+    mdp VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE utilisateur (
-  id serial primary key,
-  nom VARCHAR(40) NOT NULL,
-  prenom VARCHAR(40) NOT NULL,
-  email VARCHAR(70) NOT NULL,
-  fonction VARCHAR(20) CHECK (fonction IN ('operationnel', 'chef de projet', 'ressources humaines')) NOT NULL,
-  mdp VARCHAR(40) NOT NULL,
-  
+CREATE TABLE tache (
+    id_tache INT AUTO_INCREMENT PRIMARY KEY,
+    id_utilisateur INT NOT NULL,
+    libelle VARCHAR(100) NOT NULL,
+    description TEXT,
+    date_debut DATETIME NOT NULL,
+    date_fin DATETIME NOT NULL,
+    importance VARCHAR(10) CHECK (importance IN ('faible', 'moyen', 'fort')) NOT NULL,
+    statut VARCHAR(20) CHECK (statut IN ('en cours', 'fait', 'en retard')) NOT NULL
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE --suppression simplifié
+);
 
+CREATE TABLE groupe (
+    id_groupe INT AUTO_INCREMENT PRIMARY KEY,
+    nom_groupe VARCHAR(100) NOT NULL,
+    description_groupe VARCHAR(500)
+);
 
-create table administrateurGroupe (
+CREATE TABLE groupe_membre (
+    id_groupe INT NOT NULL,
+    id_utilisateur INT NOT NULL,
+    date_inscription DATE NOT NULL DEFAULT (CURRENT_DATE),
+    PRIMARY KEY (id_groupe, id_utilisateur), --création de la clé primaire par les deux clé étrangères 
+    FOREIGN KEY (id_groupe) REFERENCES groupe(id_groupe) ON DELETE CASCADE,  --suppression simplifié
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE  --suppression simplifié
+);
+
+create table groupe_administrateur (
     id serial primary key,
 );
 
-create table Groupe (
+
 
   
