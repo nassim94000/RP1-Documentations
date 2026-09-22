163f300d172ccc19c06ea2a13cdf8f3556d1d035
SIOpRaphael <raphael.pichegru.sio@gmail.com>
2026-09-22 11:26:20 +0200

    Update synchro.sql

--- synchro.sql
+++ synchro.sql
@@ -1,20 +1,18 @@
---su -
---mariadb
 DROP DATABASE IF EXISTS synchro;
+DROP USER IF EXISTS 'johndoe'@'localhost';
+
 CREATE DATABASE synchro;
-DROP USER IF EXISTS  'johndoe'@'localhost';
-CREATE USER 'johndoe'@'localhost' identified BY 'azerty';
+CREATE USER 'johndoe'@'localhost' IDENTIFIED BY 'azerty';
 GRANT ALL PRIVILEGES ON synchro.* TO 'johndoe'@'localhost';
 FLUSH PRIVILEGES;
---exit;
---maridb -h localhost -u johndoe -p;
-use synchro;
+
+USE synchro;
 
 CREATE TABLE utilisateur (
     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     prenom VARCHAR(100) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL, -- verification email
     fonction VARCHAR(30) CHECK (fonction IN ('operationnel', 'chef de projet', 'ressources humaines')) NOT NULL,
     mdp VARCHAR(255) NOT NULL
 );
@@ -28,7 +26,7 @@ CREATE TABLE tache (
     date_fin DATETIME NOT NULL,
     importance VARCHAR(10) CHECK (importance IN ('faible', 'moyen', 'fort')) NOT NULL,
     statut VARCHAR(20) CHECK (statut IN ('en cours', 'fait', 'en retard')) NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE --suppression simplifié
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE -- suppression simplifiée
 );
 
 CREATE TABLE groupe (
@@ -41,13 +39,8 @@ CREATE TABLE groupe_membre (
     id_groupe INT NOT NULL,
     id_utilisateur INT NOT NULL,
     date_inscription DATE NOT NULL DEFAULT (CURRENT_DATE),
-    est_admin BOOLEAN NOT NULL,
-    PRIMARY KEY (id_groupe, id_utilisateur), --création de la clé primaire par les deux clé étrangères 
-    FOREIGN KEY (id_groupe) REFERENCES groupe(id_groupe) ON DELETE CASCADE,  --suppression simplifié
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE  --suppression simplifié
+    est_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id_groupe, id_utilisateur), --  création de la primaires avec les deux clés étrangéres
+    FOREIGN KEY (id_groupe) REFERENCES groupe(id_groupe) ON DELETE CASCADE, -- suppression simplifiée
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE -- suppression simplifiée
 );
-
-
-
-
-  
