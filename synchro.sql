886562a6f0ac6a1f46c5d3671ea4762f58db4cb4
SIOpRaphael <raphael.pichegru.sio@gmail.com>
2026-09-22 11:30:07 +0200

    Update synchro.sql

--- synchro.sql
+++ synchro.sql
@@ -12,8 +12,8 @@ CREATE TABLE utilisateur (
     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     prenom VARCHAR(100) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL, -- verification email
-    fonction VARCHAR(30) CHECK (fonction IN ('operationnel', 'chef de projet', 'ressources humaines')) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    fonction ENUM('operationnel', 'chef de projet', 'ressources humaines') NOT NULL,
     mdp VARCHAR(255) NOT NULL
 );
 
@@ -24,9 +24,9 @@ CREATE TABLE tache (
     description TEXT,
     date_debut DATETIME NOT NULL,
     date_fin DATETIME NOT NULL,
-    importance VARCHAR(10) CHECK (importance IN ('faible', 'moyen', 'fort')) NOT NULL,
-    statut VARCHAR(20) CHECK (statut IN ('en cours', 'fait', 'en retard')) NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE -- suppression simplifiée
+    importance ENUM('faible', 'moyen', 'fort') NOT NULL,
+    statut ENUM('en cours', 'fait', 'en retard') NOT NULL,
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
 );
 
 CREATE TABLE groupe (
@@ -40,7 +40,7 @@ CREATE TABLE groupe_membre (
     id_utilisateur INT NOT NULL,
     date_inscription DATE NOT NULL DEFAULT (CURRENT_DATE),
     est_admin BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id_groupe, id_utilisateur), --  création de la primaires avec les deux clés étrangéres
-    FOREIGN KEY (id_groupe) REFERENCES groupe(id_groupe) ON DELETE CASCADE, -- suppression simplifiée
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE -- suppression simplifiée
+    PRIMARY KEY (id_groupe, id_utilisateur),
+    FOREIGN KEY (id_groupe) REFERENCES groupe(id_groupe) ON DELETE CASCADE,
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
 );
